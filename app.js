6f897d80656d36e635ea25316395cfde6977674a
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-01-12 11:45:17 +0000

    PORT ADDED

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = precess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Static Files
 app.use(express.static('public'));
@@ -23,4 +23,4 @@ app.use('/', newsRouter);
 app.use('/article', newsRouter);
 
 // Listen on port 5000
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${PORT}`));
